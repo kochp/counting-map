1f5e2ea7826468ac2dc404016e161972abfc691b
Philipp Koch <Philipp.Koch@senacor.com>
2016-01-28 19:16:14 +0100

    Add mixed TestMethode.

--- src/test/java/de/phienor/lesson/CountingHashMapTest.java
+++ src/test/java/de/phienor/lesson/CountingHashMapTest.java
@@ -19,6 +19,21 @@ import java.util.Map;
 public class CountingHashMapTest {
 
     @Test
+    public void shouldCountSingleAndMultiplePut(){
+        CountingHashMap countingMap = new CountingHashMap();
+        countingMap.put("test1","Test1");
+        countingMap.put("test2","Test2");
+        countingMap.put("test3","Test3");
+
+        Map input = new HashMap();
+        input.put("test4","Test4");
+        input.put("test5","Test5");
+        countingMap.putAll(input);
+
+        // Should be 5 but will be 7
+        Assert.assertEquals(5,countingMap.getCounter());
+    }
+    @Test
     public void shouldCountSinglePut(){
         CountingHashMap countingMap = new CountingHashMap();
         countingMap.put("test1","Test1");
